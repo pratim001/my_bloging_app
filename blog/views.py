ab449f7cc55ce32c5f2c756f835c98f3d3393c42
Pratim <pratimghosh90@gmail.com>
2020-06-05 18:09:38 +0530

    This commit adds the facility to show the related posts by tags

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .forms import EmailPostForm , CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
-
+from django.db.models import Count
 class PostListView(ListView):
     queryset=Post.published.all()
     context_object_name='posts'
@@ -26,7 +26,11 @@ class PostListView(ListView):
     return render(request,'blog/post/list.html',{'page':page,'posts':posts})'''
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,slug = post,status = 'published',publish__year = year, publish__month = month,publish__day =day)
-    
+    #List of similar posts
+    post_tag_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
     #editing is started from here
     comments = post.comments.filter(active=True)
     new_comment = None
@@ -39,7 +43,8 @@ def post_detail(request,year,month,day,post):
     else:
         comment_form = CommentForm()
     
-    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form})
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form,'similar_posts':similar_posts})
+
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status='published')
