4e77969e391abe14130be778e30fe1b4be1c088e
Pratim <pratimghosh90@gmail.com>
2020-06-05 17:06:13 +0530

    this adds the tagging facility to the blog

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
 
 class PublishedManager(models.Manager):
@@ -24,7 +25,8 @@ class Post(models.Model):
 
     odjects = models.Manager()
     published = PublishedManager()
-
+    
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from .models import Post,Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm , CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+
 class PostListView(ListView):
     queryset=Post.published.all()
     context_object_name='posts'
@@ -54,4 +57,20 @@ def post_share(request,post_id):
         form = EmailPostForm()
     return render(request,'blog/post/share.html',{'post':post,'form':form,'sent':sent})
 
-
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list,3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num.num_pages)
+    return render(request,'blog/post/list.html',{'page':page,'posts':posts,'tag':tag})
+     
+    
